558cb988f06d4e00eced9817306fcf70e3f08872
yoboom <1369276645@qq.com>
2023-03-31 15:04:34 +0800

    deploy

---
+++ assets/guide_i18n.md.f6d80b4c.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,N as l}from"./chunks/framework.212bf628.js";const A=JSON.parse('{"title":"国际化","description":"","frontmatter":{},"headers":[],"relativePath":"guide/i18n.md","lastUpdated":1680246248000}'),p={name:"guide/i18n.md"},o=l("",18),e=[o];function t(c,r,y,D,i,F){return a(),n("div",null,e)}const d=s(p,[["render",t]]);export{A as __pageData,d as default};
